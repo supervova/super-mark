7f6c6cd24d560fc7ce23ab91eaf16d1abea9580e
Vladimir Nikishin <nickysheen@yandex.ru>
2024-12-02 11:19:49 +0100

    content: Update services sections of homepage

--- build/server.js
+++ build/server.js
@@ -69,4 +69,45 @@ function serveBs(done) {
   done();
 }
 
+// const serveBs = (done) => {
+//   bsInstance.init({
+//     server: {
+//       baseDir: root.dest.site, // Основная директория - dist
+//     },
+//     port: 9000,
+//     notify: false,
+
+//     // Extensionless URLs
+//     middleware: [
+//       (req, res, next) => {
+//         const baseDir = root.dest.site;
+
+//         // Если URL корневой, перенаправляем на index.html
+//         if (req.url === '/') {
+//           req.url = '/index.html';
+//         } else {
+//           // Проверка: если URL заканчивается на `/`, ищем index.html
+//           const dirPath = join(baseDir, req.url, 'index.html');
+//           if (req.url.endsWith('/') && existsSync(dirPath)) {
+//             req.url += 'index.html';
+//           } else if (!/\.\w+$/.test(req.url)) {
+//             // Если нет расширения, проверяем, существует ли index.html в директории
+//             const potentialIndex = join(baseDir, req.url, 'index.html');
+//             if (existsSync(potentialIndex)) {
+//               req.url += '/index.html';
+//             } else {
+//               // Иначе просто добавляем .html к URL
+//               req.url += '.html';
+//             }
+//           }
+//         }
+
+//         next();
+//       },
+//     ],
+//   });
+//   watchFiles();
+//   done();
+// };
+
 export { serve, serveBs, watchFiles };
